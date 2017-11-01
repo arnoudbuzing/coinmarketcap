d4c95f5abaf71ba71a2343597f08608ad19bc114
Arnoud Buzing <arnoudb@wolfram.com>
2017-11-01 13:02:09 -0500

    updates

---
+++ CoinMarketCap/PacletInfo.m
@@ -0,0 +1,12 @@
+Paclet[
+  Name -> "BugTracker",
+  Description -> "BugTracker is a Wolfram Language package to track bugs",
+  Creator -> "Arnoud Buzing",
+  Publisher -> "Wolfram Research",
+  Version -> "0.0.1",
+  MathematicaVersion -> "11.0+",
+  Extensions -> {
+   { "Documentation", Language -> "English"},
+   { "Application", Context -> "BugTracker`" }
+  }
+]
